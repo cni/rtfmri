4895f7c0b05de4690ee8239356fdd18b41ad5c98
Bob Dougherty <bobd@stanford.edu>
2013-03-13 14:40:37 -0700

    replaying history-- this is Kiefer's first version

--- rtfmri.py
+++ rtfmri.py
@@ -4,24 +4,28 @@
 
 import os
 import sys
-import glob
 import time
 import Queue as queue
+import shlex
 import signal
 import argparse
 import datetime
 import threading
+import subprocess
+import collections
 
 import dicom
+
+QUANTIZATION_CORRECTION = datetime.timedelta(seconds=2)
 
 
 class DicomFinder(threading.Thread):
 
     """DicomFinder finds the latest dicom files and pushes them into the dicom queue as dicom objects."""
 
-    def __init__(self, exam_path, dicom_q, interval):
+    def __init__(self, exam_dir, dicom_q, interval):
         super(DicomFinder, self).__init__()
-        self.exam_path = exam_path
+        self.exam_dir = exam_dir
         self.dicom_q = dicom_q
         self.interval = interval
         self.alive = True
@@ -30,25 +34,47 @@ class DicomFinder(threading.Thread):
         self.alive = False
 
     def run(self):
-        files_dict = {}
+        #find_cmd_str = 'find %s -type f -newermt "%s"'
+        find_cmd_str = 'find %s -type f'
+        series_dir_cmd = 'find %s -mindepth 1 -maxdepth 1 -exec stat -c "%%Y;%%n" {} +' % self.exam_dir
+        #hist_len = int((2 * QUANTIZATION_CORRECTION.seconds / self.interval.total_seconds()) + 0.5)
+        #history = collections.deque([set() for i in range(hist_len)], maxlen=hist_len)
+        last_check = datetime.datetime.now()
+        filenames_dict = {}
         while self.alive:
-            before_check = datetime.datetime.now()
-            series_path = max(glob.glob(os.path.join(exam_path, 's*')), key=lambda d: os.stat(d).st_mtime)
-            current_files = os.listdir(series_path)
-            new_files = set(current_files) - files_dict.setdefault(series_path, set())
-            for filename in new_files.copy():
-                try:
-                    dcm = dicom.read_file(os.path.join(series_path, filename))
-                    if not len(dcm.PixelData) == 2 * dcm.Rows * dcm.Columns:
-                        raise Exception
-                except:
-                    new_files.remove(filename)  # try this one again next time
-                else:
-                    self.dicom_q.put(dcm)
-            files_dict[series_path] |= new_files
+            all_series_dirs = subprocess.check_output(shlex.split(series_dir_cmd), stderr=subprocess.STDOUT).split()
+            all_series_dirs = dict([ex.split(';') for ex in all_series_dirs])
+            series_dir = all_series_dirs[max(all_series_dirs)]
 
-            sleeptime = (before_check + self.interval - datetime.datetime.now()).total_seconds()
-            print '%s: %4d (%3d) [%f]' % (os.path.basename(series_path), len(files_dict[series_path]), len(new_files), sleeptime)
+            this_check = datetime.datetime.now()
+            #find_cmd = find_cmd_str % (series_dir, last_check-QUANTIZATION_CORRECTION)
+            find_cmd = find_cmd_str % (series_dir)
+
+            try:
+                #find_out = subprocess.check_output(shlex.split(find_cmd), stderr=subprocess.STDOUT).split()
+                find_out = [os.path.join(series_dir, d) for d in os.listdir(series_dir)]
+            except subprocess.CalledProcessError:
+                print 'Error while checking for new files'
+            else:
+                last_check = this_check
+                #filenames = set(find_out) - set.union(*history)
+                filenames = set(find_out) - filenames_dict.setdefault(series_dir, set())
+                for filename in filenames.copy():
+                    try:
+                        dcm = dicom.read_file(filename)
+                        if not len(dcm.PixelData) == 2 * dcm.Rows * dcm.Columns:
+                            raise Exception
+                    except:
+                        filenames.remove(filename)  # try this one again next time
+                    else:
+                        #print ' dicom %5s in' % dcm.InstanceNumber
+                        print filename
+                        self.dicom_q.put(dcm)
+                #history.append(filenames)
+                filenames_dict[series_dir] |= filenames
+
+            sleeptime = (this_check + self.interval - datetime.datetime.now()).total_seconds()
+            print '%4d (%3d): %s (%f)' % (len(filenames_dict[series_dir]), len(filenames), last_check, sleeptime)
             if sleeptime > 0:
                 time.sleep(sleeptime)
 
@@ -104,10 +130,11 @@ class Analyzer(threading.Thread):
 class ArgumentParser(argparse.ArgumentParser):
 
     def __init__(self):
-        super(ArgumentParser, self).__init__(formatter_class=argparse.RawTextHelpFormatter)
-        self.description  = 'Facilitate real-time fMRI.\n\n'
-        self.description += 'Use NFS mount options "noac,lookupcache=none" to avoid client-side caching.'
-        self.add_argument('dicom_path', help='path to dicom root directory')
+        super(ArgumentParser, self).__init__()
+        self.description = """
+            Facilitate real-time fMRI.
+            """
+        self.add_argument('dicom_dir', help='path to dicom root directory')
         self.add_argument('-i', '--interval', type=float, default=1.0, help='interval between checking for new files')
 
 
@@ -117,9 +144,12 @@ if __name__ == '__main__':
     dicom_q = queue.Queue()
     volume_q = queue.Queue()
 
-    exam_path = max(glob.glob(os.path.join(args.dicom_path, 'p*/e*')), key=lambda d: os.stat(d).st_mtime)
+    exam_dir_cmd = 'find %s -mindepth 2 -maxdepth 2 -exec stat -c "%%Y;%%n" {} +' % args.dicom_dir
+    all_exam_dirs = subprocess.check_output(shlex.split(exam_dir_cmd), stderr=subprocess.STDOUT).split()
+    all_exam_dirs = dict([ex.split(';') for ex in all_exam_dirs])
+    exam_dir = all_exam_dirs[max(all_exam_dirs)]
 
-    dicom_finder = DicomFinder(exam_path, dicom_q, datetime.timedelta(seconds=args.interval))
+    dicom_finder = DicomFinder(exam_dir, dicom_q, datetime.timedelta(seconds=args.interval))
     volumizer = Volumizer(dicom_q, volume_q)
     analyzer = Analyzer(volume_q)
 
