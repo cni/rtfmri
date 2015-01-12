4dc7c2743bd657065b8afe14ced2d29f76d8dae8
Michael Waskom <mwaskom@stanford.edu>
2015-01-12 15:11:29 -0800

    Add test for motion analyzer registration method

--- rtfmri/tests/test_analyzers.py
+++ rtfmri/tests/test_analyzers.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
-from nose import SkipTest
+import nibabel as nib
+
 import nose.tools as nt
 import numpy.testing as npt
 
@@ -47,3 +48,21 @@ class TestMotionAnalyzer(object):
         assert a.new_scanner_run(dict(exam=2, series=2, acquisition=6))
         assert a.new_scanner_run(dict(exam=1, series=3, acquisition=6))
         assert a.new_scanner_run(dict(exam=1, series=2, acquisition=7))
+
+    def test_compute_registration(self):
+
+        x, y, = np.meshgrid(np.linspace(-100, 100, 100),
+                            np.linspace(-100, 100, 100))
+
+        e = (x ** 2 / 40 ** 2 + y ** 2 / 50 ** 2 < 1).astype(np.float)
+        e *= np.sin(x / 4)
+        e1 = np.zeros((100, 100, 10))
+        e1[..., 5] = e
+        e2 = np.roll(e1, 5, axis=1)
+
+        eim1 = nib.Nifti1Image(e1, np.eye(4))
+        eim2 = nib.Nifti1Image(e2, np.eye(4))
+
+        a = anal.MotionAnalyzer(None, None)
+        T = a.compute_registration(eim1, eim2)
+        npt.assert_array_almost_equal(T.translation, [0, 5, 0], 1)
